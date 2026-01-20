6c347b7f4c6c8550444dd260ad72f3992bf5bb07
viEcho <1156947957@qq.com>
2026-01-20 16:53:22 +0800

    新增：新增用户相关表

--- market-backend/src/main/java/com/market/business/global/Result.java
+++ market-backend/src/main/java/com/market/business/global/Result.java
@@ -8,8 +8,6 @@ import lombok.Data;
 
 /**
  * @description: 统一返回封装
- * @author: echo
- * @date: 2021/5/22
  */
 @Data
 @Schema(name = "全局的统一返回结果")
@@ -30,30 +28,30 @@ public class Result {
     public Result() {
         this.success = ResponseCodeEnum.SUCCESS.getSuccess();
         this.code = ResponseCodeEnum.SUCCESS.getCode();
-        this.msg= ResponseCodeEnum.SUCCESS.getDesc();
+        this.msg = ResponseCodeEnum.SUCCESS.getDesc();
     }
 
-    public Result(Object data){
+    public Result(Object data) {
         this.success = ResponseCodeEnum.SUCCESS.getSuccess();
         this.code = ResponseCodeEnum.SUCCESS.getCode();
         this.msg = ResponseCodeEnum.SUCCESS.getDesc();
         this.data = data;
     }
 
-    public Result(int code,String msg,Object data){
+    public Result(int code, String msg, Object data) {
         this.success = ResponseCodeEnum.SUCCESS.getSuccess();
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public  Result (ResponseCodeEnum responseCodeEnum){
+    public Result(ResponseCodeEnum responseCodeEnum) {
         this.success = responseCodeEnum.getSuccess();
         this.code = responseCodeEnum.getCode();
         this.msg = responseCodeEnum.getDesc();
     }
 
-    public  Result (ResponseCodeEnum responseCodeEnum,Object data){
+    public Result(ResponseCodeEnum responseCodeEnum, Object data) {
         this.success = responseCodeEnum.getSuccess();
         this.code = responseCodeEnum.getCode();
         this.msg = responseCodeEnum.getDesc();
@@ -62,50 +60,43 @@ public class Result {
 
     /**
      * @description: ok返回
-     * @author: echo
-     * @date: 2021/5/22
-     * @param:
-     * @return: com.vbills.modules.common.Result
      */
-    public static Result success(){
+    public static Result success() {
         return new Result()
-        .success(ResponseCodeEnum.SUCCESS.getSuccess())
-        .code(ResponseCodeEnum.SUCCESS.getCode())
-        .msg(ResponseCodeEnum.SUCCESS.getDesc());
+                .success(ResponseCodeEnum.SUCCESS.getSuccess())
+                .code(ResponseCodeEnum.SUCCESS.getCode())
+                .msg(ResponseCodeEnum.SUCCESS.getDesc());
     }
-
 
 
     /**
      * @description: error返回
-     * @author: echo
-     * @date: 2021/5/22
-     * @param:
-     * @return: com.vbills.modules.common.Result
      */
-    public static Result fail(){
+    public static Result fail() {
         return new Result()
-        .success(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getSuccess())
-        .code(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getCode())
-        .msg(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getDesc());
+                .success(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getSuccess())
+                .code(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getCode())
+                .msg(ResponseCodeEnum.UNKNOWN_REASON_ERROR.getDesc());
     }
 
 
-    public Result success(Boolean success){
+    public Result success(Boolean success) {
         this.setSuccess(success);
         return this;
     }
-    public Result msg(String msg){
+
+    public Result msg(String msg) {
         this.setMsg(msg);
         return this;
     }
-    public Result code(Integer code){
+
+    public Result code(Integer code) {
         this.setCode(code);
         return this;
     }
-    public Result data(Object obj){
-        this.setData(obj);
-        return this;
+
+    public static Result data(Object obj) {
+        return new Result(obj);
     }
 
 }
